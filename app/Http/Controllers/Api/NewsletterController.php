6619d381de00ff13c0ffe10c41557f427d3c66c1
belal-allala <belalallala810@gmail.com>
2025-04-10 09:52:23 +0100

    feat(newsletter): Implement basic CRUD logic in NewsletterController

--- app/Http/Controllers/Api/NewsletterController.php
+++ app/Http/Controllers/Api/NewsletterController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Newsletter;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class NewsletterController extends Controller
 {
@@ -12,7 +14,8 @@ class NewsletterController extends Controller
      */
     public function index()
     {
-        //
+        $newsletters = Newsletter::paginate(10);
+        return response()->json($newsletters);
     }
 
     /**
@@ -20,30 +23,54 @@ class NewsletterController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $newsletter = Newsletter::create($request->all());
+
+        return response()->json($newsletter, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Newsletter $newsletter)
     {
-        //
+        return response()->json($newsletter);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Newsletter $newsletter)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'title' => 'string|max:255',
+            'content' => 'string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $newsletter->update($request->all());
+
+        return response()->json($newsletter);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Newsletter $newsletter)
     {
-        //
+        $newsletter->delete();
+
+        return response()->json(null, 204);
     }
 }
