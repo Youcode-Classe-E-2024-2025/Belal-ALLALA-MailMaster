fe41b85f20a3e646281f918a0c0323a5e472f7b0
belal-allala <belalallala810@gmail.com>
2025-04-10 14:12:41 +0100

    feat: Create Campaign entity (model and migration)

---
+++ database/migrations/2025_04_10_131030_create_campaigns_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('campaigns', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('newsletter_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->string('subject');
+            $table->timestamp('sent_at')->nullable();
+            $table->enum('status', ['draft', 'pending', 'sent', 'failed'])->default('draft');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('campaigns');
+    }
+};
