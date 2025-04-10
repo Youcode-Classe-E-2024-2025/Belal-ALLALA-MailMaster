a4539c21d1b3dc6efa1ee0d0e33ea771e2ad3e2a
belal-allala <belalallala810@gmail.com>
2025-04-10 09:43:45 +0100

    feat: Define API routes for newsletter CRUD operations using apiResource

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\SubscriberController;
+use App\Http\Controllers\Api\NewsletterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('subscribers', SubscriberController::class);
+Route::apiResource('newsletters', NewsletterController::class);
