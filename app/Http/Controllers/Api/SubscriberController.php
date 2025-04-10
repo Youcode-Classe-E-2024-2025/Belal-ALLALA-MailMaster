335f413e8d315234964016e15fb4af7692f23804
belal-allala <belalallala810@gmail.com>
2025-04-10 09:52:46 +0100

    feat(subscriber): Implement basic CRUD logic in SubscriberController

--- app/Http/Controllers/Api/SubscriberController.php
+++ app/Http/Controllers/Api/SubscriberController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Subscriber;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class SubscriberController extends Controller
 {
@@ -12,7 +14,8 @@ class SubscriberController extends Controller
      */
     public function index()
     {
-        //
+        $subscribers = Subscriber::paginate(10);
+        return response()->json($subscribers);
     }
 
     /**
@@ -20,30 +23,54 @@ class SubscriberController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|unique:subscribers,email',
+            'name' => 'nullable|string|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $subscriber = Subscriber::create($request->all());
+
+        return response()->json($subscriber, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Subscriber $subscriber)
     {
-        //
+        return response()->json($subscriber);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Subscriber $subscriber)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'email' => 'email|unique:subscribers,email,' . $subscriber->id,
+            'name' => 'nullable|string|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $subscriber->update($request->all());
+
+        return response()->json($subscriber);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Subscriber $subscriber)
     {
-        //
+        $subscriber->delete();
+
+        return response()->json(null, 204);
     }
 }
